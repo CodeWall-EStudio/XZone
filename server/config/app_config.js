8743a59176b0a07c5b288f00a59dc7442874b6ad
azrael <azrael@imatlas.com>
2014-02-24 23:54:02 +0800

    fix

--- server/config/app_config.js
+++ server/config/app_config.js
@@ -24,7 +24,7 @@ exports.PDF_TYPES = ['application/pdf'];
 // 权限的常量
 // 权限 0x0 普通 0x1 小组管理员 0x2 部门管理员 0x4 管理员 0x8 系统管理员
 exports.AUTH_USER = 0x0;
-exports.AUTH_GROUP_MANAGER = 0X1;
-exports.AUTH_DEPART_MANAGER = 0X2;
-exports.AUTH_MANAGER = 0X4;
-exports.AUTH_SYS_MANAGER = 0X8;
+exports.AUTH_GROUP_MANAGER = 0x1;
+exports.AUTH_DEPART_MANAGER = 0x2;
+exports.AUTH_MANAGER = 0x4;
+exports.AUTH_SYS_MANAGER = 0x8;
