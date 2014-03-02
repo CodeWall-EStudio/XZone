9538f3d3702c38b5dfb8765932d683d904cfda1c
azrael <azrael@imatlas.com>
2014-03-02 10:55:01 +0800

    bugfix

--- server/models/resource.js
+++ server/models/resource.js
@@ -46,11 +46,13 @@ exports.updateRef = function(resId, value, callback){
                 
         if(doc && doc.ref <= 0){ // 引用为0了, 删除文件
             console.log('>>>resource ref=0, delete it: ' + doc._id);
-            db.resource.findAndRemove({ _id: doc._id }, [], callback);
             // 还要删除具体文件
             fs.unlink(path.join(config.FILE_SAVE_ROOT, config.FILE_SAVE_DIR, doc.path), function(err){
                 if(err){
                     console.error('>>>delete file: ', err);
+                    callback(err, null);
+                }else{
+                    db.resource.findAndRemove({ _id: doc._id }, [], callback);
                 }
             });
         }else{
