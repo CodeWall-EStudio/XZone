2319d1f0523877fbb28e0bd474c1377094b74f3a
azrael <azrael@imatlas.com>
2014-06-04 19:47:13 +0800

    sizegroup加上重名检测

--- server/routes/sizegroup.js
+++ server/routes/sizegroup.js
@@ -12,19 +12,32 @@ exports.create = function(req, res){
     var loginUser = req.loginUser;
 
     var createParams = us.extend({}, req.parameter);
+
     createParams.updateUserId = loginUser._id;
 
-    mSizegroup.create(createParams, function(err, result){
+    // 检查重名
+    mSizegroup.getSizegroup({ name: createParams.name }, function(err, result){
+
         if(err){
             return res.json({ err: result || ERR.SERVER_ERROR, msg: err});
         }
-        res.json({
-            err: ERR.SUCCESS,
-            result: {
-                data: result
+        if(doc){
+            return res.json({ err: ERR.DUPLICATE, msg: 'name duplicate'});
+        }
+
+        mSizegroup.create(createParams, function(err, result){
+            if(err){
+                return res.json({ err: result || ERR.SERVER_ERROR, msg: err});
             }
+            res.json({
+                err: ERR.SUCCESS,
+                result: {
+                    data: result
+                }
+            });
         });
     });
+
 
 };
 
@@ -34,6 +47,10 @@ exports.modify = function(req, res){
     var loginUser = req.loginUser;
 
     var sizegroup = params.sizegroupId;
+    var ep = new EventProxy();
+    ep.fail(function(err, errorCode){
+        return res.json({ err: errorCode || ERR.SERVER_ERROR, msg: err});
+    });
 
     var doc = {};
     doc.updateUser = new DBRef('user', loginUser._id);
@@ -50,16 +67,30 @@ exports.modify = function(req, res){
         doc.isDefault = params.isDefault;
     }
 
-    mSizegroup.modify({ _id: sizegroup._id, type: sizegroup.type }, doc, function(err, result){
-        if(err){
-            return res.json({ err: result || ERR.SERVER_ERROR, msg: err});
+    // 检查重名
+    if(doc.name){
+        mSizegroup.getSizegroup({ name: doc.name }, ep.doneLater('checkNameDone'));
+    }else{
+        ep.emitLater('checkNameDone', null);
+    }
+
+    ep.on('checkNameDone', function(doc){
+        if(doc){
+            return res.json({ err: ERR.DUPLICATE, msg: 'name duplicate'});
         }
-        res.json({
-            err: ERR.SUCCESS,
-            result: {
-                data: result
+
+        mSizegroup.modify({ _id: sizegroup._id, type: sizegroup.type }, doc, function(err, result){
+            if(err){
+                return res.json({ err: result || ERR.SERVER_ERROR, msg: err});
             }
+            res.json({
+                err: ERR.SUCCESS,
+                result: {
+                    data: result
+                }
+            });
         });
+
     });
 
 };
