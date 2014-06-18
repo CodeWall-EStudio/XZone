468f39452c43810bb42d5c19d30a19ae6ea83eab
katans <myhorde@vip.qq.com>
2014-06-18 22:45:11 +0800

    回复 文件....

---
+++ server/models/organization.js
@@ -0,0 +1,203 @@
+var ObjectID = require('mongodb').ObjectID;
+var DBRef = require('mongodb').DBRef;
+var us = require('underscore');
+var EventProxy = require('eventproxy');
+
+var db = require('./db');
+var config = require('../config');
+var ERR = require('../errorcode');
+var mUser = require('../models/user');
+var Util = require('../util');
+var Logger = require('../logger');
+
+exports.create = function(params, callback) {
+
+    var doc = {
+        name: params.name,
+        order: params.order || 0,
+        parent: params.parentId && new DBRef('department', params.parentId),
+        creator: new DBRef('user', params.creator),
+        createTime: Date.now(),
+        updateTime: Date.now()
+    };
+
+    db.department.save(doc, function(err) {
+        callback(err, err || doc);
+    });
+
+};
+
+exports.modify = function(query, doc, callback) {
+
+    doc.updateTime = Date.now();
+
+    db.department.findAndModify(query, [], {
+        $set: doc
+    }, {
+        'new': true
+    }, callback);
+
+};
+
+exports.delete = function(query, callback) {
+
+    Logger.info('[organization] delete: ', query);
+
+
+
+    db.department.remove(query, callback);
+
+};
+
+exports.getUsers = function(departmentId, options, callback) {
+
+    var fetchDetail = options.fetchDetail || false;
+
+    db.departuser.find({
+        'department.$id': departmentId
+    }, function(err, depUsers) {
+
+        if (fetchDetail && depUsers && depUsers.length) {
+            ep.after('fetchDepartUsers', depUsers.length, function(list) {
+                var users = us.compact(list);
+
+                callback(null, users);
+            });
+
+            depUsers.forEach(function(doc) {
+
+                // 过滤掉测试用户
+                db.user.findOne({
+                    _id: doc.user.oid,
+                    test: null,
+                    status: 0
+                }, ep.group('fetchDepartUsers'));
+
+            });
+        } else {
+            callback(err, err || depUsers);
+        }
+
+    });
+
+};
+
+exports.getChildren = function(departmentId, options, callback) {
+
+    db.department.find({
+        'parent.$id': departmentId
+    }, callback);
+
+};
+
+exports.addUser = function(params, callback) {
+
+    db.departuser.findOne ({
+        'user.$id': params.userId,
+        'department.$id': params.organizationId
+    }, function(err, result){
+        if(result){
+            return callback(err, result);
+        }
+        var doc = {
+            user: new DBRef('user', params.userId),
+            department: new DBRef('department', params.organizationId)
+        };
+        db.department.save(doc, function(err){
+            callback(err, err || doc);
+        });
+    });
+};
+
+exports.removeUser = function(params, callback) {
+
+    db.departuser.remove({
+        'user.$id': params.userId,
+        'department.$id': params.organizationId
+    }, {
+        multi: true
+    }, callback);
+};
+
+
+function fetchDepartments(dep, callback){
+    var ep = new EventProxy();
+    ep.fail(callback);
+
+    // 1. 先查询该部门的用户
+    db.departuser.find({ 'department.$id': dep._id }, ep.doneLater('getUsers'));
+
+    ep.on('getUsers', function(depUsers){
+
+        dep.users = [];
+
+        ep.after('fetchDepartUsers', depUsers.length, function(list){
+            dep.users = us.compact(list);
+
+            ep.emit('getUsersDone');
+        });
+
+        depUsers.forEach(function(doc){
+
+            // 过滤掉测试用户
+            db.user.findOne({ _id: doc.user.oid, test: null, status: 0 }, ep.group('fetchDepartUsers'));
+
+        });
+
+    });
+
+    // 2. 查询该部门的子部门
+    db.search('department', { 'parent.$id': dep._id }, { order: { order: 1 } }, ep.doneLater('getDeps'));
+
+    ep.on('getDeps', function(total, deps){
+        deps = deps || [];
+        dep.children = deps;
+
+        ep.after('fetchChildDeparts', deps.length, function(){
+            ep.emit('getDepartsDone');
+        });
+
+        deps.forEach(function(doc){
+            fetchDepartments(doc, ep.group('fetchChildDeparts'));
+        });
+
+    });
+
+    ep.all('getUsersDone', 'getDepartsDone', function(){
+
+        callback(null, dep);
+    });
+
+}
+
+exports.getOrganizationTree = function(params, callback){
+
+    var ep = new EventProxy();
+    ep.fail(callback);
+
+    db.department.findOne({ parent: null }, ep.doneLater('getOrgRoot'));
+
+    ep.on('getOrgRoot', function(root){
+
+        if(!root){
+            return callback('can\'t find the root of organization');
+        }
+
+        // 查询一级部门
+        db.search('department', { 'parent.$id': root._id }, { order: { order: 1 } }, function(err, total, docs){
+            if(err){
+                return callback(err);
+            }
+            root.children = docs;
+
+            ep.after('fetchChildDeparts', docs.length, function(){
+                callback(null, root);
+            });
+
+            docs.forEach(function(doc){
+                fetchDepartments(doc, ep.group('fetchChildDeparts'));
+            });
+            
+        });
+    });
+};
---
+++ server/routes/organization.js
@@ -0,0 +1,193 @@
+var ObjectID = require('mongodb').ObjectID;
+var DBRef = require('mongodb').DBRef;
+var us = require('underscore');
+var EventProxy = require('eventproxy');
+
+var db = require('../models/db');
+var config = require('../config');
+var ERR = require('../errorcode');
+var mUser = require('../models/user');
+var mOrganization = require('../models/organization');
+var Util = require('../util');
+
+exports.create = function(req, res){
+    var params = req.parameter;
+    var parent = params.parentId;
+    var order = params.order;
+    var name = params.name;
+
+    var loginUser = req.loginUser;
+
+    var ep = new EventProxy();
+    ep.fail(function(err, errCode){
+        return res.json({ err: errCode || ERR.SERVER_ERROR, msg: err });
+    });
+
+    if(parent){
+
+        mOrganization.getUsers(parent._id, { fetchDetail: false }, ep.doneLater('getUsersDone'));
+    }else{
+        ep.emitLater('getUsersDone', []);
+    }
+
+    ep.on('getUsersDone', function(users){
+        if(users && users.length){
+            return ep.emit('error', 'this organization is not empty, it has user', ERR.NOT_EMPTY);
+        }
+
+        mOrganization.create({
+            name: name,
+            order: order,
+            parentId: parent && parent._id,
+            creator: loginUser._id
+        }, ep.done('createDone'));
+    });
+
+    ep.on('createDone', function(dep){
+        res.json({
+            err: ERR.SUCCESS,
+            result: {
+                data: dep
+            }
+        });
+    });
+
+};
+
+exports.addUser = function(req, res){
+    var params = req.parameter;
+    var user = params.userId;
+    var organ = params.organizationId;
+
+    var ep = new EventProxy();
+    ep.fail(function(err, errCode){
+        return res.json({ err: errCode || ERR.SERVER_ERROR, msg: err });
+    });
+
+
+    mOrganization.getChildren(organ._id, {}, ep.doneLater('getChildrenDone'));
+
+    ep.on('getChildrenDone', function(list){
+        if(list && list.length){
+            return ep.emit('error', 'addUser failure, this organization has child', ERR.UNINSERTABLE);
+        }
+        mOrganization.addUser({
+            userId: user._id,
+            organizationId: organ._id
+        }, ep.done('addUserDone'));
+    });
+
+    ep.on('addUserDone', function(){
+        res.json({
+            err: ERR.SUCCESS,
+            msg: 'ok'
+        });
+    });
+
+};
+
+exports.removeUser = function(req, res){
+    var params = req.parameter;
+    var user = params.userId;
+    var organ = params.organizationId;
+
+    var ep = new EventProxy();
+    ep.fail(function(err, errCode){
+        return res.json({ err: errCode || ERR.SERVER_ERROR, msg: err });
+    });
+
+    mOrganization.removeUser({
+        userId: user._id,
+        organizationId: organ._id
+    }, ep.done('removeUserDone'));
+
+    ep.on('removeUserDone', function(){
+        res.json({
+            err: ERR.SUCCESS,
+            msg: 'ok'
+        });
+    });
+};
+
+exports.modify = function(req, res){
+    var params = req.parameter;
+    var name = params.name;
+    var order = params.order;
+    var organization = params.organizationId;
+
+    var doc = {};
+    if(name){
+        doc.name = name;
+    }
+    if('order' in params){
+        doc.order = order;
+    }
+
+    mOrganization.modify({ _id: organization._id }, doc, function(err, result){
+        if(err){
+            return res.json({ err: errCode || ERR.SERVER_ERROR, msg: err });
+        }
+        res.json({
+            err: ERR.SUCCESS,
+            result: {
+                data: result
+            }
+        });
+    });
+};
+
+exports.delete = function(req, res){
+    var params = req.parameter;
+
+    var organ = params.organizationId;
+    var ep = new EventProxy();
+
+    mOrganization.getChildren(organ._id, {}, ep.doneLater('getChildrenDone'));
+
+    mOrganization.getUsers(organ._id, { fetchDetail: false }, ep.doneLater('getUsersDone'));
+
+
+    ep.all('getChildrenDone', 'getUsersDone', function(deps, users){
+        if(deps && deps.length){
+            return ep.emit('error', 'this organization has child', ERR.NOT_EMPTY);
+        }
+        if(users && users.length){
+            return ep.emit('error', 'this organization has user', ERR.NOT_EMPTY);
+        }
+
+        mOrganization.delete({
+            _id: organ._id
+        }, ep.done('deleteDone'));
+    });
+
+    ep.on('deleteDone', function(){
+        res.json({
+            err: ERR.SUCCESS,
+            msg: 'ok'
+        });
+    });
+};
+
+exports.tree = function(req, res){
+    var params = req.parameter;
+
+    // params.fetchUser = false;
+
+    mOrganization.getOrganizationTree(params, function(err, data){
+        if(err){
+            res.json({ err: ERR.SERVER_ERROR, msg: err});
+        }else{
+            res.json({
+                err: ERR.SUCCESS,
+                result: {
+                    data: data
+                }
+            });
+        }
+    });
+};
+
+exports.search = function(req, res){
+    // TODO
+    res.json({ err: ERR.SERVER_ERROR, msg: 'not ready'});
+};
