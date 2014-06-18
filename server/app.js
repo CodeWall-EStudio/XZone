cbf776aa988254ac8fa956005897346bed903c68
katans <myhorde@vip.qq.com>
2014-06-18 23:14:53 +0800

    去掉后台的登录检查

--- server/app.js
+++ server/app.js
@@ -42,8 +42,9 @@ app.use(app.router);
 app.use(express.static(path.join(__dirname, '../web'), { maxAge: config.STATIC_FILE_EXPIRES }));
 
 // 检查是否登录, 如果没有登录, 跳转到登录页
-app.all('/', routes.checkAuthAndLogin);
-app.all(config.INDEX_PAGE, routes.checkAuthAndLogin);
+//app.all('/', routes.checkAuthAndLogin);
+//app.all(config.INDEX_PAGE, routes.checkAuthAndLogin);
+app.all('/', config.INDEX_PAGE);
 
 // 设置跨域请求头
 app.all('/*', routes.setXHR2Headers);
