518be233e9d7310934823eecdcef2e433c924a89
katans <myhorde@vip.qq.com>
2014-12-14 23:18:43 +0800

    change

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,19 @@ var gulp = require('gulp'),
     minifycss = require('gulp-minify-css'),
     replace = require('gulp-replace'),
     watch = require('gulp-watch'),
-    inline = require('gulp-inline'),
+    //inline = require('gulp-inline'),
     htmlreplace = require('gulp-html-replace'),
-    sass = require('gulp-sass'),
+    //sass = require('gulp-sass'),
     concat = require('gulp-concat'),
-    es = require('event-stream'),
+    //es = require('event-stream'),
     compass = require('gulp-compass');
 var rjs = require('gulp-requirejs');
 
 var src = './src';
 var dist = './dist';
 
-var cssList = ["./src/css/bootstrap.css","./src/css/jquery.ui.min.css","./src/css/base.css","./src/css/common.css","./src/css/icon.css","./src/css/tips.css","./src/css/list.css","./src/css/upload.css","./src/css/manage.css","./src/css/share.css","./src/css/scroll.css","./src/css/type.css","./src/css/messenger.css","./src/css/messenger-spinner.css","./src/css/messenger-theme-air.css","./src/css/messenger-theme-block.css","./src/css/messenger-theme-flat.css","./src/css/messenger-theme-future.css","./src/css/messenger-theme-ice.css","./src/css/jquery.jqplot.css","./src/css/pickmeup.min.css","./src/css/jquery.plupload.queue.css",'./src/css/review.win.css'];
-var mcssList = ["./src/css/bootstrap.css","./src/css/jquery.ui.min.css","./src/css/base.css","./src/css/common.css","./src/css/icon.css","./src/css/tips.css","./src/css/list.css","./src/css/upload.css","./src/css/manage.css","./src/css/share.css","./src/css/scroll.css","./src/css/type.css","./src/css/messenger.css","./src/css/messenger-spinner.css","./src/css/messenger-theme-air.css","./src/css/messenger-theme-block.css","./src/css/messenger-theme-flat.css","./src/css/messenger-theme-future.css","./src/css/messenger-theme-ice.css","./src/css/jquery.jqplot.css","./src/css/pickmeup.min.css",'./src/css/review.win.css'];
+var cssList = ["./src/css/bootstrap.css","./src/css/jquery.ui.min.css","./src/css/base.css","./src/css/common.css","./src/css/icon.css","./src/css/tips.css","./src/css/list.css","./src/css/upload.css","./src/css/manage.css","./src/css/share.css","./src/css/scroll.css","./src/css/type.css","./src/css/review.win.css","./src/css/messenger.css","./src/css/messenger-spinner.css","./src/css/messenger-theme-air.css","./src/css/messenger-theme-block.css","./src/css/messenger-theme-flat.css","./src/css/messenger-theme-future.css","./src/css/messenger-theme-ice.css","./src/css/jquery.jqplot.css","./src/css/pickmeup.min.css","./src/css/jquery.plupload.queue.css"];
+var mcssList = ["./src/css/bootstrap.css","./src/css/jquery.ui.min.css","./src/css/base.css","./src/css/common.css","./src/css/icon.css","./src/css/tips.css","./src/css/list.css","./src/css/upload.css","./src/css/manage.css","./src/css/share.css","./src/css/scroll.css","./src/css/type.css","./src/css/messenger.css","./src/css/messenger-spinner.css","./src/css/messenger-theme-air.css","./src/css/messenger-theme-block.css","./src/css/messenger-theme-flat.css","./src/css/messenger-theme-future.css","./src/css/messenger-theme-ice.css","./src/css/jquery.jqplot.css","./src/css/pickmeup.min.css"];
 var rcssList = ["./src/css/bootstrap.css","./src/css/review.css","./src/css/player/video-js.min.css"];
 var jqlib = ['./src/js/lib/jquery/jq.validate.js','./src/js/lib/jquery/jquery.pickmeup.js','./src/js/lib/jquery/jquery.jqplot.min.js','./src/js/lib/jquery/jqplot.funnelRenderer.js','./src/js/lib/jquery/jqplot.pieRenderer.js'];
 var jqupload = ['./src/js/lib/jquery/moxie.js','./src/js/lib/jquery/plupload.dev.js','./src/js/lib/jquery/jquery.plupload.queue.js'];
@@ -28,34 +28,34 @@ var bootstrap = ['./src/js/lib/bootstrap.min.js','./src/js/lib/messenger.min.js'
 
 gulp.task('requirejs',function(){
 
-		//baseUrl: 'path/to/your/base/file.js',
+    //baseUrl: 'path/to/your/base/file.js',
         //out: 'FILENAME\_TO\_BE\_OUTPUTTED',
-	  rjs({
-		name : '../main',
-		baseUrl: './src/js/school',
-		out: 'main.js',
-  	    mainConfigFile: './src/js/main.js',
-		shim: {}
-	  })
-	  .pipe(gulp.dest('./web/js'));
+    rjs({
+    name : '../main',
+    baseUrl: './src/js/school',
+    out: 'main.js',
+        mainConfigFile: './src/js/main.js',
+    shim: {}
+    })
+    .pipe(gulp.dest('./web/js'));
 
-	  rjs({
-		name : '../review',
-		baseUrl: './src/js/school',
-		out: 'review.js',
-  	    mainConfigFile: './src/js/review.js',
-		shim: {}
-	  })
-	  .pipe(gulp.dest('./web/js'));
+    rjs({
+    name : '../review',
+    baseUrl: './src/js/school',
+    out: 'review.js',
+        mainConfigFile: './src/js/review.js',
+    shim: {}
+    })
+    .pipe(gulp.dest('./web/js'));
 
-	  rjs({
-		name : '../manage',
-		baseUrl: './src/js/manage',
-		out: 'manage.js',
-  	    mainConfigFile: './src/js/manage.js',
-		shim: {}
-	  })
-	  .pipe(gulp.dest('./web/js'));
+    rjs({
+    name : '../manage',
+    baseUrl: './src/js/manage',
+    out: 'manage.js',
+        mainConfigFile: './src/js/manage.js',
+    shim: {}
+    })
+    .pipe(gulp.dest('./web/js'));
 
 });
 
@@ -71,7 +71,10 @@ gulp.task('concat',function(){
     .pipe(gulp.dest('./web/js/lib'));  
   gulp.src(bootstrap)
     .pipe(concat('bootstrap.js'))
-    .pipe(gulp.dest('./web/js/lib'));      
+    .pipe(gulp.dest('./web/js/lib')); 
+
+  gulp.src('./src/js/login.js')
+    .pipe(gulp.dest('./web/js/')); 
 
   gulp.src(cssList)
     .pipe(concat('main.css'))
@@ -85,6 +88,10 @@ gulp.task('concat',function(){
     .pipe(concat('review.css'))
     .pipe(minifycss())
     .pipe(gulp.dest('./web/css/'));
+  gulp.src(rcssList)
+    .pipe(concat('bootstrap.css'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./web/css/'));    
 
 });
 
@@ -93,12 +100,14 @@ gulp.task('build',function(){
     .pipe(replace('%TimeStamp%',new Date().getTime()))
     .pipe(htmlreplace({    
       'maincss' : 'css/main.css?t='+new Date().getTime(),
+      'logincss' : 'css/bootstrap.css?t='+new Date().getTime(),
       'reviewcss' : 'css/review.css?t='+new Date().getTime(),
       'managecss' : 'css/manage.css?t='+new Date().getTime(),
       'jqlib' : 'js/lib/jqlib.js',
       'jqupload' : 'js/lib/jqupload.js',
       'bootstrap' : 'js/lib/bootstrap.js',
-      'reviewlib' : 'js/lib/reviewlib.js'
+      'reviewlib' : 'js/lib/reviewlib.js',
+      'loginjs' : 'js/login.js',
     }))
     .pipe(gulp.dest('./web/'));  
 });
